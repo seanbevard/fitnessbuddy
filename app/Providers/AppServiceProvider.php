2fd1fe518a954528fa0f6dc965e39ea393dfda37
Sean Bevard <seanbevard@gmail.com>
2017-06-26 18:24:12 -0400

    fixing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,13 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
       Schema::defaultStringLength(191);
-      $url->forceSchema('https');
+      if(env('APP_ENV') !== 'local')
+          {
+              $url->forceSchema('https');
+          }
     }
 
     /**
